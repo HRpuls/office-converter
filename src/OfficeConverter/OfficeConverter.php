d6f833d6a437f3a372e576aad3804a5317ebb442
Diae-Louali <68733422+Diae-Louali@users.noreply.github.com>
2022-02-21 12:21:46 +0100

    test timeout command

--- src/OfficeConverter/OfficeConverter.php
+++ src/OfficeConverter/OfficeConverter.php
@@ -50,6 +50,7 @@ class OfficeConverter
 
         $outdir = $this->tempPath;
         $shell = $this->exec($this->makeCommand($outdir, $outputExtension, $timeout));
+        echo '<pre>$shell : ', print_r($shell, true) ,'</pre>';
         if (0 != $shell['return']) {
             throw new OfficeConverterException('Convertion Failure! Contact Server Admin.');
         }
@@ -127,7 +128,6 @@ class OfficeConverter
         $timeOutCmd = $timeout !== 0 ? 'timeout 0.001s echo "TIMED OUT IN 0.001s"' : 'echo "DID NOT TIME OUT"';
         $oriFile = escapeshellarg($this->file);
         $outputDirectory = escapeshellarg($outputDirectory);
-
         return "{$timeOutCmd} {$this->bin} --headless --convert-to {$outputExtension} {$oriFile} --outdir {$outputDirectory}";
     }
 
